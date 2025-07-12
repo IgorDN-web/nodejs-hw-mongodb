2c06f9acf96f0e02596445b05d9b8473b3af0a20
IgorD <avtosklokyiv@gmail.com>
2025-07-12 09:25:59 +0300

    hw

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { setupServer } from './server.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
+
+const start = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+
+start();
