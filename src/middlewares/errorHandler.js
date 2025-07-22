7e95ee1389525773cb48279689c03a407f23d8b4
IgorD <avtosklokyiv@gmail.com>
2025-07-22 16:12:08 +0300

    Fix error

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,11 +1,10 @@
-const errorHandler = (err, req, res, next) => {
+export const errorHandler = (err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || "Something went wrong";
 
   res.status(status).json({
     status,
     message,
+    data: message,
   });
 };
-
-module.exports = { errorHandler };
