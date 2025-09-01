cb2653183e41fd70d19a44504e979e750a4e3c40
IgorD <avtosklokyiv@gmail.com>
2025-09-01 14:04:28 +0300

    fixcontact

--- src/schemas/contactSchema.js
+++ src/schemas/contactSchema.js
@@ -1,17 +1,20 @@
+// FILE: src/schemas/contactSchemas.js
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email(),
-  phone: Joi.string(),
-  contactType: Joi.string().valid("personal", "work", "other").required(),
-  isFavorite: Joi.boolean(),
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
+  contactType: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email().optional(),
+  isFavourite: Joi.boolean().optional(),
+  photo: Joi.string().uri().optional(), // URL загруженного изображения
 });
 
 export const updateContactSchema = Joi.object({
-  name: Joi.string(),
-  email: Joi.string().email(),
-  phone: Joi.string(),
-  contactType: Joi.string().valid("personal", "work", "other"),
-  isFavorite: Joi.boolean(),
-}).min(1);
+  name: Joi.string().min(3).max(20).optional(),
+  phoneNumber: Joi.string().min(3).max(20).optional(),
+  contactType: Joi.string().min(3).max(20).optional(),
+  email: Joi.string().email().optional(),
+  isFavourite: Joi.boolean().optional(),
+  photo: Joi.string().uri().optional(), // URL загруженного изображения
+});
